2fc8cdb1bf73b6653955ceae0531ea48d62def6e
katlyn-whyte <114199065+katlyn-whyte@users.noreply.github.com>
2024-04-09 15:28:18 +1000

    Edit the layout of a single page post.

--- starter_theme/inc/template-tags.php
+++ starter_theme/inc/template-tags.php
@@ -27,7 +27,7 @@ if ( ! function_exists( 'starter_theme_posted_on' ) ) :
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'starter_theme' ),
+			esc_html_x( 'Published on  %s', 'post date', 'starter_theme' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
@@ -43,44 +43,19 @@ if ( ! function_exists( 'starter_theme_posted_by' ) ) :
 	function starter_theme_posted_by() {
 		$byline = sprintf(
 			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'starter_theme' ),
+			esc_html_x( ' Written by %s', 'post author', 'starter_theme' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
 		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
-	}
-endif;
-
-if ( ! function_exists( 'starter_theme_entry_footer' ) ) :
-	/**
-	 * Prints HTML with meta information for the categories, tags and comments.
-	 */
-	function starter_theme_entry_footer() {
-		// Hide category and tag text for pages.
-		if ( 'post' === get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'starter_theme' ) );
-			if ( $categories_list ) {
-				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'starter_theme' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
-
-			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'starter_theme' ) );
-			if ( $tags_list ) {
-				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'starter_theme' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
-		}
-
-		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			echo '<span class="comments-link">';
 			comments_popup_link(
 				sprintf(
 					wp_kses(
 						/* translators: %s: post title */
-						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'starter_theme' ),
+						__( ' Leave a Comment<span class="screen-reader-text"> on %s</span>', 'starter_theme' ),
 						array(
 							'span' => array(
 								'class' => array(),
@@ -93,11 +68,12 @@ if ( ! function_exists( 'starter_theme_entry_footer' ) ) :
 			echo '</span>';
 		}
 
+
 		edit_post_link(
 			sprintf(
 				wp_kses(
 					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'starter_theme' ),
+					__( ' Edit <span class="screen-reader-text">%s</span>', 'starter_theme' ),
 					array(
 						'span' => array(
 							'class' => array(),
@@ -109,8 +85,38 @@ if ( ! function_exists( 'starter_theme_entry_footer' ) ) :
 			'<span class="edit-link">',
 			'</span>'
 		);
+
 	}
 endif;
+
+if ( ! function_exists( 'starter_theme_entry_footer' ) ) :
+	/**
+	 * Prints HTML with meta information for the categories, tags and comments.
+	 */
+	function starter_theme_entry_footer() {
+		// Hide tag text for pages.
+		if ( 'post' === get_post_type() ) {
+
+
+			/* translators: used between list items, there is a space after the comma */
+			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'starter_theme' ) );
+			if ( $tags_list ) {
+				/* translators: 1: list of tags. */
+				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'starter_theme' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			}
+		}
+	}
+endif;
+
+function starter_theme_category_list() {
+	/* translators: used between list items, there is a space after the comma */
+	$categories_list = get_the_category_list( esc_html__( ', ', 'starter_theme' ) );
+	if ( $categories_list ) {
+		/* translators: 1: list of categories. */
+		printf( '<span class="cat-links">' . esc_html__( '%1$s', 'starter_theme' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+
+}
 
 if ( ! function_exists( 'starter_theme_post_thumbnail' ) ) :
 	/**

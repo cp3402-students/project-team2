6c908dfa08ea45c2f17bf43100448d1ac6e081c6
katlyn-whyte <114199065+katlyn-whyte@users.noreply.github.com>
2024-04-12 16:20:12 +1000

    Create footer customise ability on WordPress.

--- starter_theme/footer.php
+++ starter_theme/footer.php
@@ -11,28 +11,26 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-
-
- <?php	wp_nav_menu(array('theme_location' => 'Social Media Menu'	));  ?>
-
+<footer id="colophon" class="site-footer">
+		
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'starter_theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'starter_theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'starter_theme' ), 'starter_theme', '<a href="http://underscores.me/">Group 2</a>' );
-				?>
+			<div class="copyright">
+				&copy; U3A
+			</div>
+			<?php
+			// Get footer text from Customizer
+			$footer_text = get_theme_mod('footer_text', '');
+			if (!empty($footer_text)) {
+				echo '<div class="custom-footer-text">' . esc_html($footer_text) . '</div>';
+			}
+			?>
+			<div class="footer-social">
+				<a href="https://www.facebook.com/people/U3A-Online/100057154791844/"><i class="fab fa-facebook"></i></a> 
+			</div>
+			<div class="footer-logo">
+				<img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt="Logo"> 
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+	
+	
